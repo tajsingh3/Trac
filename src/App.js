02761acc3707939f47b156e15ffcc33ec8dc70fe
tajsingh3 <tajsingh3@gmail.com>
2018-07-28 12:09:15 -0400

    paginate table finished

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Route } from 'react-router-dom';
 import TracTable from './components/TracTable/TracTable';
 import DetailedViewTable from './components/DetailedViewTable/DetailedViewTable';
 import SimpleTable from './components/SimpleTable/SimpleTable';
+import DetailedView from './components/DetailedViewTable/DetailedView';
 
 class App extends Component {
   render() {
     return (
         <div>
-          <Route path='/' exact component={TracTable}/>
-          <Route path='/detailed-view-table' exact component={DetailedViewTable}/>      
-          <Route path='/simple' exact component={SimpleTable}/>      
+          <Route path='/' exact component={SimpleTable}/>
+          <Route path='/detailed-view-table' exact component={DetailedView}/>         
         </div>
     );
   }
